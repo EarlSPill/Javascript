0243a341c397946709a3fa590826485bbefb3b15
Earl Alexus Serafica <earlalexus@gmail.com>
2024-07-24 19:23:20 +0800

    Add Data Types assignment

--- Javascript Fundamentals - Part 1/assignments.js
+++ Javascript Fundamentals - Part 1/assignments.js
@@ -1,3 +1,5 @@
+// Values and Variables
+
 let country = 'Philippines';
 let continent = 'Asia';
 let population = '115,600,000'
@@ -5,3 +7,12 @@ let population = '115,600,000'
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data Types
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);    
+console.log(typeof language);
