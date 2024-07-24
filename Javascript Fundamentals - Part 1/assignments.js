c58f1b5376801a9c0e3e4fba9a14af649ead8d03
Earl Alexus Serafica <earlalexus@gmail.com>
2024-07-24 19:32:46 +0800

    Add let, const, and var assignment

--- Javascript Fundamentals - Part 1/assignments.js
+++ Javascript Fundamentals - Part 1/assignments.js
@@ -1,18 +1,21 @@
 // Values and Variables
 
-let country = 'Philippines';
-let continent = 'Asia';
-let population = '115,600,000'
+const country = 'Philippines';
+const continent = 'Asia';
+const population = '115,600,000'
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
 // Data Types
-let isIsland = true;
+const isIsland = true;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);    
 console.log(typeof language);
+
+// let, const, and var
+language = 'Tagalog'
